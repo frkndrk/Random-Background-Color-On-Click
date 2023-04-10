b7bab4655487885cb998c4dc3d4d6e8c1ba26820
Furkan Durak <furkandurak22@hotmail.com>
2023-04-10 13:03:04 +0300

    second version with unlimited colors

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const colors = ["red", "blue", "green", "yellow", "gray", "black", "purple", "pink", "orange"];
-
 document.querySelector("#click").onclick = function() {
-    for(var i = 0; i < colors.length; i++) {
-        let randomm = Math.round(Math.random() * 9);
-        document.body.style.backgroundColor = colors[randomm];
-    }
+    document.body.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + 
+    ',' + Math.round(Math.random() * 255) + 
+    ',' + Math.round(Math.random() * 255) + ')';
 }
     
